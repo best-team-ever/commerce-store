6e18e8906cb29ea3d98071944308378992f568bd
Jean-Philippe Bornier <jeanphilippe.bornier@decathlon.com>
2018-06-11 15:32:36 +0200

    first commit before rebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TopNav from './header/TopNav';
+import MainNav from './header/MainNav';
+import HamburgerMenu from './header/HamburgerMenu';
+import Banner from './banner/Banner';
+import Footer from './footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      	<header class="header">
+          <TopNav />
+          <MainNav />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="fs_menu_overlay"></div>
+        <HamburgerMenu />
+        <div class="main_slider" />
+
+        <Banner />
+
+        <Footer/>
       </div>
     );
   }
