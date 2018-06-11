d919cfa429bbcdd9ef9fddf3b71e1b6ec5439264
OLIVIER MASUREL <olivier.masurel@decathlon.COM>
2018-06-11 17:55:22 +0200

    screen cart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Cart from './modules/cart/cart.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toto: "",
+    };
+    this.return = this.return.bind (this);
+    this.validCart = this.validCart.bind (this);
+  }
+
+  return = () => {
+    console.log("return...");
+  };
+
+  validCart = () => {
+    console.log("valid cart");
+  };
+
+  increment = () => {
+    console.log("plus +");
+  };
+
+  decrement = () => {
+    console.log("moins -");
+  };
+
+  deleteItem = () => {
+    console.log("deleteItem");
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,12 +40,21 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <span>
+          <Cart
+            return={this.return}
+            validCart={this.validCart}
+            increment={this.increment}
+            decrement={this.decrement}
+            deleteItem={this.deleteItem}
+          />
+        </span>
       </div>
     );
   }
 }
+
+
+
 
 export default App;
