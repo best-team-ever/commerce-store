781510335bab1c8f5297fdb898e383d180f15b4b
Jean-Philippe Bornier <jeanphilippe.bornier@decathlon.com>
2018-06-13 11:54:10 +0200

    bug correction

--- src/modules/products/ProductDetail.js
+++ src/modules/products/ProductDetail.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { fetchProductDetail } from "../../store/actions/productDetailAction";
