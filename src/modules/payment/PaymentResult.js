4d1230df0f870689753a2a5fb6527f18a6e27de8
Jean-Philippe Bornier <jeanphilippe.bornier@decathlon.com>
2018-06-15 14:47:37 +0200

    remove query-string node-module

--- src/modules/payment/PaymentResult.js
+++ src/modules/payment/PaymentResult.js
@@ -4,7 +4,12 @@ class PaymentResult extends Component{
   render(){
     return (
       <div className="PaymentResult">
-        <h1>Success!</h1>
+        <div className="container">
+          <div className="alert alert-success h3" role="alert">
+            Thank you for your payment.
+          </div>
+          <p>We make every effort to deliver as soon as possible... see you soon !</p>
+        </div>
       </div>
     )
   }
