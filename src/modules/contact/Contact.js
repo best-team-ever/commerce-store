4c1866981dfd0e86836f599b2ddbe199f2d1a599
Masurel Olivier <38031286+omasur@users.noreply.github.com>
2018-09-17 10:41:14 +0200

    Update Contact.js

--- src/modules/contact/Contact.js
+++ src/modules/contact/Contact.js
@@ -19,76 +19,76 @@ class Contact extends Component {
               <div className="product-item">
                 <div className="product discount product_filter">
                     <div className="product_image">
-                      <img src="./images/Yuanqin.png" class="img-thumbnail" alt="Yuanquin"/>
+                      //<img src="./images/Yuanqin.png" class="img-thumbnail" alt="Yuanquin"/>
                     </div>
                     <div className="favorite favorite_left"></div>
 
                     <div className="product_info">
                       <h6 className="product_name">
-                        Yuanqin DENG
+                        Yuanqin
                       </h6>
                       <div className="product_price">Shanghai</div>
                     </div>
                 </div>
                 <div className="red_button add_to_cart_button">
-                  <a onClick={console.log("toto")}>yuanqin.deng@decathlon.com</a>
+                  <a onClick={console.log("toto")}>yd@dk.com</a>
                 </div>
               </div>
 
               <div className="product-item">
                 <div className="product discount product_filter">
                     <div className="product_image">
-                      <img src="./images/Mat.png" class="img-thumbnail" alt="Mat"/>
+                     // <img src="./images/Mat.png" class="img-thumbnail" alt="Mat"/>
                     </div>
                     <div className="favorite favorite_left"></div>
 
                     <div className="product_info">
                       <h6 className="product_name">
-                        Matthieu DEBACK
+                        Matt
                       </h6>
                       <div className="product_price">Campus</div>
                     </div>
                 </div>
                 <div className="red_button add_to_cart_button">
-                  <a onClick={console.log("toto")}>matthieu.deback@decathlon.com</a>
+                  <a onClick={console.log("toto")}>md@dk.com</a>
                 </div>
               </div>
 
               <div className="product-item">
                 <div className="product discount product_filter">
                     <div className="product_image">
-                      <img src="./images/JPhi.png" class="img-thumbnail" alt="jphi"/>
+                     // <img src="./images/JPhi.png" class="img-thumbnail" alt="jphi"/>
                     </div>
                     <div className="favorite favorite_left"></div>
 
                     <div className="product_info">
                       <h6 className="product_name">
-                        Jean Philippe BORNIER
+                        JeanPhi
                       </h6>
                       <div className="product_price">Campus</div>
                     </div>
                 </div>
                 <div className="red_button add_to_cart_button">
-                  <a onClick={console.log("toto")}>jeanphilippe.bornier@decathlon.com</a>
+                  <a onClick={console.log("toto")}>jpb@dk.com</a>
                 </div>
               </div>
 
               <div className="product-item">
                 <div className="product discount product_filter">
                     <div className="product_image2">
-                      <img src="./images/Oliv.png" class="img-thumbnail" alt="Oliv"/>
+                     // <img src="./images/Oliv.png" class="img-thumbnail" alt="Oliv"/>
                     </div>
                     <div className="favorite favorite_left"></div>
 
                     <div className="product_info">
                       <h6 className="product_name">
-                        Olivier MASUREL
+                        Oliv
                       </h6>
                       <div className="product_price">Campus</div>
                     </div>
                 </div>
                 <div className="red_button add_to_cart_button">
-                  <a onClick={console.log("toto")}>olivier.masurel@decathlon.com</a>
+                  <a onClick={console.log("toto")}>om@dk.com</a>
                 </div>
               </div>
 
